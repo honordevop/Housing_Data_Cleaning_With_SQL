074d1c7848b46a72f0479e81a01de9eaf4ecf4e2
honordevop <ogunladestephen20@gmail.com>
2022-07-12 02:01:44 +0100

    Query: Altered table schema to change SaleDate format

--- Housing-Data_DataCleaning.sql
+++ Housing-Data_DataCleaning.sql
@@ -26,3 +26,17 @@ SELECT TOP (1000) [UniqueID ]
 
 Select CAST(Saledate AS DATE) 
 FROM [ProjectPortfolio].[dbo].[NashvilleHousing];
+
+--Update content SaleDate column to DATE fromat
+Update NashvilleHousing
+SET SaleDate = CAST(Saledate AS DATE)
+
+
+--Alter table schema to change SaleDate column to DATE fromat
+ALTER TABLE NashvilleHousing
+ALTER COLUMN SaleDate DATE;
+
+
+--View the SaleDate column as altered
+Select Saledate
+FROM [ProjectPortfolio].[dbo].[NashvilleHousing];
